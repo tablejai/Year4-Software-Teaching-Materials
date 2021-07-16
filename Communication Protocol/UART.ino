72cdc4d88625c758873b7788e029658822747ff5
Johnathan <jjjlaw@connect.ust.hk>
2021-07-16 17:57:49 +0800

    More refactor, add temperature param

--- Communication Protocol/UART.ino
+++ Communication Protocol/UART.ino
@@ -1,9 +1,7 @@
 const int START_BYTE = 0x59;
 
-void getTFminiData(int* distance, int* strength) {
-  static char i = 0;
-  char j = 0;
-  int checksum = 0; 
+void getTFminiData(int* distance, int* strength, int* temperature) {
+  static int i = 0;
   static int rx[9];
   if (Serial.available()) {  
     rx[i] = Serial.read();
@@ -12,17 +10,19 @@ void getTFminiData(int* distance, int* strength) {
     } else if (i == 1 && rx[1] != START_BYTE) {
       i = 0;
     } else if (i == 8) {
-      for(j = 0; j < 8; j++) {
+      int checksum = 0; 
+      for (int j = 0; j < 8; j++) {
         checksum += rx[j];
       }
       if (rx[8] == (checksum & 0xFF)) {
         *distance = (rx[3] << 8) + rx[2];
         *strength = (rx[5] << 8) + rx[4];
+        *temperature = (rx[7] << 8) + rx[6];
       }
       i = 0;
     } else {
       i++;
-    } 
+    }
   }
 }
 
@@ -33,12 +33,15 @@ void setup() {
 void loop() {
     int distance = 0;
     int strength = 0;
+    int temperature = 0;
 
-    getTFminiData(&distance, &strength);
+    getTFminiData(&distance, &strength, &temperature);
     if (distance) {
         Serial.print(distance);
-        Serial.print("cm\t");
-        Serial.print("strength: ");
-        Serial.println(strength);
+        Serial.print("cm");
+        Serial.print("\tstrength: ");
+        Serial.print(strength);
+        Serial.print("\ttemperature: ");
+        Serial.println(temperature);
     }
 }
